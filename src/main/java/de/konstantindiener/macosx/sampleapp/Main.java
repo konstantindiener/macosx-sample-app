4d441dfa9330b2294962901f27bb8e836205ed60
Konstantin Diener <konstantin.diener@cosee.biz>
2018-12-25 21:54:50 +0100

    Added blank main window to the application.

--- src/main/java/de/konstantindiener/macosx/sampleapp/Main.java
+++ src/main/java/de/konstantindiener/macosx/sampleapp/Main.java
@@ -1,8 +1,14 @@
 package de.konstantindiener.macosx.sampleapp;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hallo macOS");
+        MainWindow mainWindow = new MainWindow("macOS Text Edit");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setVisible(true);
+        mainWindow.setSize(new Dimension(400,300));
     }
 }
---
+++ src/main/java/de/konstantindiener/macosx/sampleapp/MainWindow.java
@@ -0,0 +1,11 @@
+package de.konstantindiener.macosx.sampleapp;
+
+import javax.swing.*;
+import java.awt.*;
+
+class MainWindow extends JFrame {
+
+    MainWindow(String title) throws HeadlessException {
+        super(title);
+    }
+}
